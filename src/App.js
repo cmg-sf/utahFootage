1b9d2a4ce03a72bed7c0229254211c9c03175b93
Lloyd Grubham <grubham@gmail.com>
2017-12-14 11:49:13 -0700

    Initial commit from script

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+
+import Footer from './components/Footer/Footer';
+
+import poster from './img/poster.jpg';
+import logo from './img/sf-logo.png';
+
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+  handleChange(val){
+    this.setState({
+      search: val
+    })
+  }
+  search(e){
+    e.preventDefault();
+    window.location.assign(`https://stockfootage.com/?s=utah+${this.state.search}&post_type=product`);
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <div className="bg-video-container">
+          <video className="bg-video" loop="1" autoPlay="1" playsInline="" muted="1" data-bgvideo="" poster={poster} data-bgvideo-fade-in="500" data-bgvideo-pause-after="120" data-bgvideo-show-pause-play="false" data-bgvideo-pause-play-x-pos="right" data-bgvideo-pause-play-y-pos="top">
+            <source src="/assets/bg-video.mp4" type="video/mp4"/>
+            <source src="/assets/bg-video.mp4" type="video/webm"/>
+            <source src="/assets/bg-video.mp4" type="video/ogg"/>
+          </video>
+        </div>
+
+        <div id="vidtop-content">
+          <header>
+            <div></div>
+            <a className="nav-link" href="https://stockfootage.com/">HOME</a>
+            <a className="nav-link" href="https://stockfootage.com/shop">SHOP</a>
+            <a className="nav-link" href="https://stockfootage.com/my-account">LOGIN</a>
+          </header>
+          <div className="interact-box">
+            <img src={logo} alt=""/>
+            <h1><span style={{color: 'rgb(188, 52, 38)'}}>Utah</span>Footage.com</h1>
+            <form className="search" onSubmit={e=>this.search(e)}>
+              <input placeholder="Search" onChange={e=>this.handleChange(e.target.value)} value={this.state.search} type="text"/>
+            </form>
+          </div>
+          <div></div>
+        </div>
+
+        <div className="info-sec">
+          <section>
+            <h1><a className="rf" href="https://en.wikipedia.org/wiki/Royalty-free">Royalty-Free</a> Stock Footage Marketplace (contact us for 4K)</h1>
+            <h4 className="sub-banner">UtahFootage.com is a division of <a style={{textDecoration:'none', color: '#999'}} href="https://stockfootage.com/">StockFootage.com</a></h4>
+          </section>
+          <div className="hr-red"/>
+          <section className="category-sec">
+            <h1>Browse by Category</h1>
+            <div className="categories">
+						<a href="https://stockfootage.com/?s=utah+Salt+lake&post_type=product" className="cat">Salt lake</a>
+						<a href="https://stockfootage.com/?s=utah+Arches+National+park&post_type=product" className="cat">Arches National park</a>
+						<a href="https://stockfootage.com/?s=utah+Zion&post_type=product" className="cat">Zion</a>
+						<a href="https://stockfootage.com/?s=utah+Sliver+lake&post_type=product" className="cat">Sliver lake</a>
+						<a href="https://stockfootage.com/?s=utah+Uintas&post_type=product" className="cat">Uintas</a>
+						<a href="https://stockfootage.com/?s=utah+Mars&post_type=product" className="cat">Mars</a>
+            </div>
+          </section>
+          <br/>
+          <div className="hr-red"/>
+          <section className="contributors">
+            <h1><span className="contrib-show">Contributors Showcase</span> - Inspired to Create</h1>
+
+
+            <div className="contributor-previews">
+              <div className="contributor-preview">
+                <iframe title="yt1" width="350" height="195" src="https://www.youtube.com/embed/1TFC-nttP3U" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt2" width="350" height="195" src="https://www.youtube.com/embed/x03OEDmPGok" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt3" width="350" height="195" src="https://www.youtube.com/embed/f2Rs8xzlvig" frameBorder="0" allowFullScreen></iframe>
+              </div>
+            </div>
+
+            <div className="mobile-contributor-previews">
+              <div className="contributor-preview">
+                <iframe title="yt4" width="250" height="150" src="https://www.youtube.com/embed/1TFC-nttP3U" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt5" width="250" height="150" src="https://www.youtube.com/embed/x03OEDmPGok" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt6" width="250" height="150" src="https://www.youtube.com/embed/f2Rs8xzlvig" frameBorder="0" allowFullScreen></iframe>
+              </div>
+            </div>
+
+            <br/>
+            <p>Download high quality royalty-free 4K and HD stock video footage clips from the most popular travel locations in Utah including:  Arches National Park, Utah Mountains, Utah Desert, Sale Lake City, Southern Utah, Utah Salt Flats, and others. Contact us about selling your own stock footage through our online community.</p>
+          </section>
+          <br/>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
